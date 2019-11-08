06ea10a90d57edb772c90c7e72cc35dfd3b3d9a4
Jose Dario Torres <dariomtorres@gmail.com>
2019-11-07 21:12:08 -0500

    Initial commit for scripts.

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+//side bar functions
+function side_open() {
+	document.getElementById("sidebar").style.display = "block";
+	document.getElementById("main").style.marginRight = "15%";
+	document.getElementById("MainFooter").style.marginRight = "16%";
+}
