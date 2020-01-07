387c98992fd795e7f89b4b02de920f5d5bf7855c
Jose Dario Torres <dariomtorres@gmail.com>
2020-01-07 02:55:13 -0500

    Addded resizing based on resolution on load and some visual faults

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,28 @@
-//side bar functions
-function side_open() {
-	document.getElementById("sidebar").style.display = "block";
-	document.getElementById("main").style.marginRight = "15%";
-	document.getElementById("MainFooter").style.marginRight = "16%";
+/*Function for acquiring resolution from browser and passing on to
+ * variables in style.css.
+ * 
+ * Portions of code acquired from:
+ * 
+*/
+function get_resolution(){
+	var width  = screen.width;
+	var height = screen.height;
+	var Home = document.getElementById('Home');
+	console.log('height' + height);
+	Home.style.setProperty('--height', height);
+}
+
+/* Function to control opening and closing of the sidebar navigation menu*/
+function sidebar_open(){
+	document.getElementById("Sidebar").style.display = "block";
+	document.getElementById("hamburger").style.display = "none";
+	document.getElementById("cross").style.display = "block";
+	document.getElementById("Menu_Button").setAttribute("onClick", "sidebar_close()");
+}
+
+function sidebar_close(){
+	document.getElementById("Sidebar").style.display = "none";
+	document.getElementById("hamburger").style.display = "block";
+	document.getElementById("cross").style.display = "none";
+	document.getElementById("Menu_Button").setAttribute("onClick", "sidebar_open()");
 }
